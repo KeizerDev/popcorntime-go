ecbf18ab7628ffb09a470561ae68fa3172a42650
Robert-Jan Keizer <robertjankeizer@gmail.com>
2017-05-24 15:30:32 +0200

    update

--- main.go
+++ main.go
@@ -7,25 +7,46 @@ import (
 
 func main() {
 	err := ui.Main(func() {
-		window := ui.NewWindow("Popcorn Time Go", 200, 100, false)
-		label := ui.NewLabel("text")
-		window.SetChild(label)
+		window := ui.NewWindow("Popcorn Time Go", 200, 100, true)
+
+        wrapper := ui.NewVerticalBox()
+       
+		window.SetChild(wrapper)
+
 		window.OnClosing(func(*ui.Window) bool {
 			ui.Quit()
 			return true
 		})
+
 		window.Show()
-		go counter(label)
+
+		go counter(wrapper)
 	})
 	if err != nil {
 		panic(err)
 	}
 }
-func counter(label *ui.Label) {
+func counter(wrapper *ui.Box) {
 	request := gorequest.New()
 	_, body, _:= request.Get("http://tv-v2.api-fetch.website/movies/1").End()
 
+
 	ui.QueueMain(func() {
-		label.SetText("number " + body)
+	
+		box := ui.NewVerticalBox()
+		box2 := ui.NewVerticalBox()
+		name := ui.NewEntry()
+        button := ui.NewButton("Greet")
+        greeting := ui.NewLabel("")
+    
+	    box.Append(ui.NewLabel("Enter your name:"), false)
+
+		name.SetText("number " + body)
+		
+        box.Append(name, false)
+        box.Append(button, false)
+        box.Append(greeting, false)
+	
+		wrapper.Append(box, false)
 	})
 }
