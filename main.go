5a0dd6d869c0210be4840062567c84da1a2378f2
Robert-Jan Keizer <robertjankeizer@gmail.com>
2017-05-24 15:45:50 +0200

    update

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+    "encoding/json"
+	"fmt"
 	"github.com/andlabs/ui"
 	"github.com/parnurzeal/gorequest"
 )
+
+type MovieResponse struct {
+    ExecutionTime string `json:"executionTime"`
+    StationBeanList []Station `json:"stationBeanList"`
+}
 
 func main() {
 	err := ui.Main(func() {
@@ -30,23 +37,35 @@ func counter(wrapper *ui.Box) {
 	request := gorequest.New()
 	_, body, _:= request.Get("http://tv-v2.api-fetch.website/movies/1").End()
 
-
-	ui.QueueMain(func() {
+    for i := range body {
+		fmt.Println(body[i])
+	}
+	// ui.QueueMain(func() {
 	
-		box := ui.NewVerticalBox()
-		box2 := ui.NewVerticalBox()
-		name := ui.NewEntry()
-        button := ui.NewButton("Greet")
-        greeting := ui.NewLabel("")
+	// 	box := ui.NewVerticalBox()
+	// 	box2 := ui.NewVerticalBox()
+	// 	name := ui.NewEntry()
+    //     button := ui.NewButton("Greet")
+    //     greeting := ui.NewLabel("")
     
-	    box.Append(ui.NewLabel("Enter your name:"), false)
+	//     box.Append(ui.NewLabel("Enter your name:"), false)
 
-		name.SetText("number " + body)
+	// 	name.SetText("number " + body)
 		
-        box.Append(name, false)
-        box.Append(button, false)
-        box.Append(greeting, false)
+    //     box.Append(name, false)
+    //     box.Append(button, false)
+    //     box.Append(greeting, false)
 	
-		wrapper.Append(box, false)
-	})
+	// 	wrapper.Append(box, false)
+	// })
+}
+
+
+func ParseMovies(body string) (*StationsResponse, error) {
+    var s = new(StationsResponse)
+    err := json.Unmarshal(body, &s)
+    if(err != nil){
+        fmt.Println("whoops:", err)
+    }
+    return s, err
 }
