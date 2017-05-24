5facf53a4ba4388f615fcebb1d7c38b6196267dd
Robert-Jan Keizer <robertjankeizer@gmail.com>
2017-05-24 13:54:23 +0200

    init

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-    "github.com/andlabs/ui"
+	"github.com/andlabs/ui"
+	"strconv"
+	"time"
 )
 
 func main() {
-    err := ui.Main(func() {
-        name := ui.NewEntry()
-        button := ui.NewButton("Greet")
-        greeting := ui.NewLabel("")
-        box := ui.NewVerticalBox()
-        box.Append(ui.NewLabel("Enter your name:"), false)
-        box.Append(name, false)
-        box.Append(button, false)
-        box.Append(greeting, false)
-        window := ui.NewWindow("Hello", 200, 100, false)
-        window.SetChild(box)
-        button.OnClicked(func(*ui.Button) {
-            greeting.SetText("Hello, " + name.Text() + "!")
-        })
-        window.OnClosing(func(*ui.Window) bool {
-            ui.Quit()
-            return true
-        })
-        window.Show()
-    })
-    if err != nil {
-        panic(err)
-    }
+	err := ui.Main(func() {
+		window := ui.NewWindow("title", 200, 100, false)
+		label := ui.NewLabel("text")
+		window.SetChild(label)
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		window.Show()
+		go counter(label)
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+func counter(label *ui.Label) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(time.Second)
+		ui.QueueMain(func() {
+			label.SetText("number " + strconv.Itoa(i))
+		})
+	}
 }
