4fa8414716c5f4baf7dcbe52cd7c7d3a169b628b
Robert-Jan Keizer <robertjankeizer@gmail.com>
2017-05-24 18:38:43 +0200

    update

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 		window := ui.NewWindow("Popcorn Time Go", 200, 100, true)
 
         wrapper := ui.NewVerticalBox()
-       
+
 		window.SetChild(wrapper)
 
 		window.OnClosing(func(*ui.Window) bool {
@@ -33,8 +33,6 @@ func main() {
 	}
 }
 func counter(wrapper *ui.Box, api core.API) {
-	//request := gorequest.New()
-	//_, body, _:= request.Get("http://tv-v2.api-fetch.website/movies/1").End()
 
 	resp, err := api.Movies(1, core.SORT_TRENDING, core.ORDER_NEWEST, "")
 	if err != nil {
@@ -42,36 +40,25 @@ func counter(wrapper *ui.Box, api core.API) {
 	}
 	
 	fmt.Println(resp)
-
-	//for i := range body {
-	//	fmt.Println(body[i])
-	//}
-	// ui.QueueMain(func() {
-	
-	// 	box := ui.NewVerticalBox()
-	// 	box2 := ui.NewVerticalBox()
-	// 	name := ui.NewEntry()
-    //     button := ui.NewButton("Greet")
-    //     greeting := ui.NewLabel("")
-    
-	//     box.Append(ui.NewLabel("Enter your name:"), false)
-
-	// 	name.SetText("number " + body)
-		
-    //     box.Append(name, false)
-    //     box.Append(button, false)
-    //     box.Append(greeting, false)
-	
-	// 	wrapper.Append(box, false)
-	// })
+	for key, value := range resp {}
+		fmt.Println(resp[i])
+	}
+	//ui.QueueMain(func() {
+	//
+	 //	box := ui.NewVerticalBox()
+	 //	box2 := ui.NewVerticalBox()
+	 //	name := ui.NewEntry()
+        //button := ui.NewButton("Greet")
+        //greeting := ui.NewLabel("")
+	//
+	 //    box.Append(ui.NewLabel("Enter your name:"), false)
+	//
+	 //	name.SetText("number " + body)
+		//
+        // box.Append(name, false)
+        // box.Append(button, false)
+        // box.Append(greeting, false)
+	//
+	 //	wrapper.Append(box, false)
+	 //})
 }
-
-
-//func ParseMovies(body string) (*StationsResponse, error) {
-//    var s = new(StationsResponse)
-//    err := json.Unmarshal(body, &s)
-//    if(err != nil){
-//        fmt.Println("whoops:", err)
-//    }
-//    return s, err
-//}
